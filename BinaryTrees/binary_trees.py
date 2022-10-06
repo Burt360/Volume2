bce89907344895fd46a1792ab7e52ad365ed0a4b
Nathan Schill <schilln@byu.edu>
2022-10-06 10:56:12 -0600

    Problems 1-3

--- BinaryTrees/binary_trees.py
+++ BinaryTrees/binary_trees.py
@@ -1,11 +1,12 @@
 # binary_trees.py
 """Volume 2: Binary Trees.
-<Name>
-<Class>
-<Date>
+Nathan Schill
+Sec. 3
+Thurs. Oct. 13, 2022
 """
 
 # These imports are used in BST.draw().
+from matplotlib import pyplot as plt
 import networkx as nx
 from networkx.drawing.nx_agraph import graphviz_layout
 
@@ -53,7 +54,25 @@ class SinglyLinkedList:
         Returns:
             (SinglyLinkedListNode): the node containing the data.
         """
-        raise NotImplementedError("Problem 1 Incomplete")
+        
+        def _step(self, data, node):
+            # Checks the given node.
+            
+            # If the node is None, the end of the list has been reached
+            # (either the list is empty or the value is not in the list).
+            if node is None:
+                raise ValueError(str(data) + " is not in the list")
+            
+            # If the node contains the value, return it.
+            elif node.value == data:
+                return node
+            
+            # Otherwise, check the next node.
+            else:
+                return _step(self, data, node.next)
+            
+        # Start the recursive search from the list's head.
+        return _step(self, data, self.head)
 
 
 class BSTNode:
@@ -118,7 +137,47 @@ class BST:
             [1, 5, 7]                           |                  (8)
             [8]                                 |
         """
-        raise NotImplementedError("Problem 2 Incomplete")
+        
+        # If the tree is empty, insert the node at the root.
+        if self.root is None:
+            self.root = BSTNode(data)
+            return self.root
+
+        # Otherwise, define a recursive function to traverse the tree.
+        def _step(current, parent=None):
+            """Recursively step through the tree until the location where the node
+            containing the data is found. If there is already a node there, raise a Value Error.
+            """
+            
+            # Base case 1: found where the node should be inserted.
+            if current is None:
+                # If the data is less than the parent's value, double link on the left of parent.
+                if data < parent.value:
+                    parent.left = BSTNode(data)
+                    parent.left.prev = parent
+                    return parent.left
+                
+                # If the data is greater than the parent's value, double link on the right of parent.
+                else:
+                    parent.right = BSTNode(data)
+                    parent.right.prev = parent
+                    return parent.right
+
+            # Base case 2: data already in tree.
+            if data == current.value:
+                raise ValueError(str(data) + " is already in the tree.")
+
+            # Recursively search left.
+            if data < current.value:
+                return _step(current.left, current)
+            
+            # Recursively search right.
+            else:
+                return _step(current.right, current)
+
+        # Start the recursion on the root of the tree.
+        return _step(self.root)
+        
 
     # Problem 3
     def remove(self, data):
@@ -151,7 +210,92 @@ class BST:
             >>> print(t2)                       | >>> t4.remove(5)
             []                                  | ValueError: <message>
         """
-        raise NotImplementedError("Problem 3 Incomplete")
+        
+        # Find the target. If the tree is empty or the value is not in the tree, find will raise a ValueError.
+        target = self.find(data)
+
+        # If the target is a leaf node:
+        if target.left is None and target.right is None:
+            # If the target is the root:
+            if self.root == target:
+                # Remove the only access to the target.
+                self.root = None
+            # If the target is to the left of its parent:
+            elif target.value < target.prev.value:
+                # Remove the parent's left link.
+                target.prev.left = None
+            # If the target is to the right of its parent:
+            else:
+                # Remove the parent's right link.
+                target.prev.right = None
+        
+        # If the target has two children:
+        elif target.left is not None and target.right is not None:
+            # Find the inorder predecessor.
+            pred = target.left
+            while pred.right is not None:
+                pred = pred.right
+
+            pred_val = pred.value
+            # Remove the in-order predecessor using remove on its value.
+            self.remove(pred_val)
+
+            # Replace the value of the target with the value of the in-order predecessor.
+            self.root.value = pred_val
+        
+        # If the target has one child (it has at least one because
+        # it's not the case that both are None and
+        # it's not the case that both are not None):
+
+        # If the target has a left child: 
+        elif target.left is not None:
+            # Promote the left child and remove links to the target.
+
+            # If the target is the root:
+            if self.root == target:
+                # Set the target's left child as the root.
+                self.root = self.root.left
+            
+            # If the target is to the left of its parent:
+            elif target.value < target.prev.value:
+                # Set the target's left (only) child as the parent's left child.
+                target.prev.left = target.left
+
+                # Set the promoted child's parent as the target's old parent.
+                target.left.prev = target.prev
+            
+            # If the target is to the right of its parent:
+            else:
+                # Set the target's left (only) child as the parent's right child.
+                target.prev.right = target.left
+
+                # Set the target's old parent as the promoted child's parent.
+                target.left.prev = target.prev
+
+        # If the target has a right child:
+        else:
+            # Promote the right child and remove links to the target.
+
+            # If the target is the root:
+            if self.root == target:
+                # Set the target's right child as the root.
+                self.root = self.root.right
+            
+            # If the target is to the left of its parent:
+            elif target.value < target.prev.value:
+                # Set the target's right (only) child as the parent's left child.
+                target.prev.left = target.right
+
+                # Set the promoted child's parent as the target's old parent.
+                target.right.prev = target.prev
+            
+            # If the target is to the right of its parent:
+            else:
+                # Set the target's right (only) child as the parent's right child.
+                target.prev.right = target.right
+
+                # Set the target's old parent as the promoted child's parent.
+                target.right.prev = target.prev
 
     def __str__(self):
         """String representation: a hierarchical view of the BST.
@@ -196,6 +340,36 @@ class BST:
         nx.draw(G, pos=graphviz_layout(G, prog="dot"), arrows=True,
                 with_labels=True, node_color="C1", font_size=8)
         plt.show()
+
+def draw_test():
+    # Figure 2.2
+    vals = [5, 2, 1, 7, 6, 8, 3]
+    a = BST()
+    for val in vals:
+        a.insert(val)
+    a.draw()
+
+    a.remove(8)
+    a.draw()
+
+    a.remove(7)
+    a.draw()
+
+    a.remove(5)
+    a.insert(2.5)
+    a.insert(2.75)
+    a.remove(3)
+
+    a.draw()
+
+
+    # Figure 2.3
+    vals = [5, 2, 7, 1, 3, 8]
+    b = BST()
+    for val in vals:
+        b.insert(val)
+
+    #b.draw()
 
 
 class AVL(BST):
@@ -317,4 +491,12 @@ def prob4():
     structure. Plot the number of elements in the structure versus the build
     and search times. Use log scales where appropriate.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    # random.shuffle to avoid collisions -> ValueErrors
+    
+    FILEPATH = 'english.txt'
+    with open(FILEPATH, 'r') as file:
+        words = [line.strip() for line in file.readlines()]
+    
+    
+
+prob4()
