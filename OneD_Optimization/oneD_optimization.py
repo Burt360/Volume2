1471b5a21d30b722389cb59aa6375e2580e288b0
Nathan Schill <schilln@byu.edu>
2023-02-06 15:41:22 -0700

    All problems

--- OneD_Optimization/oneD_optimization.py
+++ OneD_Optimization/oneD_optimization.py
@@ -1,9 +1,13 @@
 # oneD_optimization.py
 """Volume 2: One-Dimensional Optimization.
-<Name>
-<Class>
-<Date>
+Nathan Schill
+Section 2
+Thurs. Feb. 9, 2023
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize as opt
 
 
 # Problem 1
@@ -22,7 +26,30 @@ def golden_section(f, a, b, tol=1e-5, maxiter=100):
         (bool): Whether or not the algorithm converged.
         (int): The number of iterations computed.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+
+    ### Algorithm 16.1 as given in the lab PDF
+    x0 = (a+b)/2
+    phi = (1+5**(1/2))/2
+
+    converged = False
+
+    for i in range(maxiter):
+        c = (b-a)/phi
+        at = b-c
+        bt = a+c
+
+        if f(at) <= f(bt):
+            b = bt
+        else:
+            a = at
+        
+        x1 = (a+b)/2
+        if abs(x0 - x1) < tol:
+            converged = True
+            break
+        x0 = x1
+    
+    return x1, converged, i+1
 
 
 # Problem 2
@@ -41,7 +68,26 @@ def newton1d(df, d2f, x0, tol=1e-5, maxiter=100):
         (bool): Whether or not the algorithm converged.
         (int): The number of iterations computed.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+
+    # Assume failure to converge unless tolerance is reached
+    converged = False
+
+    # Iterate at most N times
+    for k in range(maxiter):
+        
+        # Compute the next iteration
+        x1 = x0 - df(x0)/d2f(x0)
+        
+        # Check for convergence
+        if np.abs(x1 - x0) < tol:
+            # Upon convergence, stop iterating
+            converged = True
+            break
+
+        # Otherwise, update the guess for the next iteration
+        x0 = x1
+    
+    return x1, converged, k+1
 
 
 # Problem 3
@@ -60,7 +106,32 @@ def secant1d(df, x0, x1, tol=1e-5, maxiter=100):
         (bool): Whether or not the algorithm converged.
         (int): The number of iterations computed.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    
+    # Assume failure to converge unless tolerance is reached
+    converged = False
+
+    # Compute first instances of df(x_{k-1}), df(x_k)
+    dfx0 = df(x0)
+    dfx1 = df(x1)
+
+    # Iterate at most N times
+    for k in range(maxiter):     
+        # Compute the next iteration
+        x2 = (x0*dfx1 - x1*dfx0) / (dfx1 - dfx0)
+        
+        # Check for convergence
+        if np.abs(x2 - x1) < tol:
+            # Upon convergence, stop iterating
+            converged = True
+            break
+
+        # Otherwise, update the xk and df(xk) for the next iteration
+        x0 = x1
+        x1 = x2
+        dfx0 = dfx1
+        dfx1 = df(x2)
+    
+    return x2, converged, k+1
 
 
 # Problem 4
@@ -80,4 +151,12 @@ def backtracking(f, Df, x, p, alpha=1, rho=.9, c=1e-4):
     Returns:
         alpha (float): Optimal step size.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    
+    ### Use Algorithm 16.2 as given in the lab PDF
+    Dfp = Df(x)@p
+    fx = f(x)
+
+    while f(x + alpha*p) > fx + c*alpha*Dfp:
+        alpha = rho*alpha
+    
+    return alpha
