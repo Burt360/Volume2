516a30c71d63b8a969f9ef9171270a5a23c3b7d5
Nathan Schill <schilln@byu.edu>
2023-04-03 21:40:00 -0600

    All problems

--- InteriorPoint_Linear/interior_point_linear.py
+++ InteriorPoint_Linear/interior_point_linear.py
@@ -1,13 +1,14 @@
 # interior_point_linear.py
 """Volume 2: Interior Point for Linear Programs.
-<Name>
-<Class>
-<Date>
+Nathan Schill
+Section 2
+Thurs. Apr. 6, 2023
 """
 
 import numpy as np
 from scipy import linalg as la
-from scipy.stats import linregress
+# from scipy.stats import linregress
+from scipy import stats
 from matplotlib import pyplot as plt
 
 
@@ -80,9 +81,134 @@ def interiorPoint(A, b, c, niter=20, tol=1e-16, verbose=False):
         x ((n, ) ndarray): The optimal point.
         val (float): The minimum value of the objective function.
     """
-    raise NotImplementedError("Problems 1-4 Incomplete")
+
+    m, n = A.shape
+
+    # Centering parameter
+    SIGMA = 1/10
+    
+    def F(x, lam, mu):
+        '''Define F as in the lab PDF'''
+        return np.concatenate((A.T @ lam + mu - c, A @ x - b, np.diag(mu) @ x))
+
+    # The top two rows of the block form of DF(x, lam, mu)
+    DF_top = np.vstack((
+                np.hstack((
+                    np.zeros((n,n)), A.T, np.eye(n,n)
+                )),
+                np.hstack((
+                    A, np.zeros((m,m)), np.zeros((m,n))
+                ))
+            ))
+    
+    def search_dir(x, lam, mu, nu):
+        '''Solve equation (16.2)'''
+        
+        e = np.ones(n)
+        
+        # DF(x, lam, mu)
+        DF = np.vstack((DF_top,
+                        np.hstack((
+                            np.diag(mu), np.zeros((n,m)), np.diag(x)
+                        ))
+                      ))
+
+        # Solve the equation
+        lu_piv = la.lu_factor(DF)
+        dir = la.lu_solve(lu_piv, -F(x, lam, mu) + np.concatenate(
+            (np.zeros(n), np.zeros(m), SIGMA * nu * e)
+            )
+        )
+        
+        # grad_x, grad_lam, grad_mu
+        return dir[:n], dir[n:n+m], dir[n+m:]
+    
+    def step_size(x, mu, grad_x, grad_mu):
+        # Check if any mu has any negative entries
+        if any(mu < 0):
+            grad_mu_lt_zero = grad_mu < 0
+            a_max = np.min(-mu[grad_mu_lt_zero]/grad_mu[grad_mu_lt_zero])
+        # If all entries of mu non-negative
+        else:
+            a_max = 1
+        
+        # Check if any x has any negative entries
+        if any(x < 0):
+            grad_x_lt_zero = grad_x < 0
+            d_max = np.min(-x[grad_x_lt_zero]/grad_x[grad_x_lt_zero])
+        # If all entries of x non-negative
+        else:
+            d_max = 1
+        
+        return min(1, 0.95*a_max), min(1, 0.95*d_max)
+
+    # Get starting point
+    x, lam, mu = starting_point(A, b, c)
+
+    for _ in range(niter):
+        # Duality measure
+        nu = x@mu / n
+
+        # Check if reached tolerance
+        if nu < tol:
+            break
+        
+        # Get search direction
+        grad_x, grad_lam, grad_mu = search_dir(x, lam, mu, nu)
+        
+        # Get step size
+        a, d = step_size(x, mu, grad_x, grad_mu)
+
+        # Step
+        x += d*grad_x
+        lam += a*grad_lam
+        mu += a*grad_mu
+    
+    return x, c@x
 
 
 def leastAbsoluteDeviations(filename='simdata.txt'):
     """Generate and show the plot requested in the lab."""
-    raise NotImplementedError("Problem 5 Incomplete")
+    
+    # Load data
+    data = np.loadtxt(filename)
+
+    # Init problem
+    m = data.shape[0]
+    n = data.shape[1] - 1
+    c = np.zeros(3*m + 2*(n + 1))
+    c[:m] = 1
+    y = np.empty(2*m)
+    y[::2] = -data[:, 0]
+    y[1::2] = data[:, 0]
+    x = data[:, 1:]
+
+    # Init more problem
+    A = np.ones((2*m, 3*m + 2*(n + 1)))
+    A[::2, :m] = np.eye(m)
+    A[1::2, :m] = np.eye(m)
+    A[::2, m:m+n] = -x
+    A[1::2, m:m+n] = x
+    A[::2, m+n:m+2*n] = x
+    A[1::2, m+n:m+2*n] = -x
+    A[::2, m+2*n] = -1
+    A[1::2, m+2*n+1] = -1
+    A[:, m+2*n+2:] = -np.eye(2*m, 2*m)
+
+    # Solve
+    sol = interiorPoint(A, y, c, niter=10)[0]
+    beta = sol[m:m+n] - sol[m+n:m+2*n]
+    b = sol[m+2*n] - sol[m+2*n+1]
+    
+    # Plot LAD
+    domain = np.linspace(0, 10, 200)
+    plt.scatter(data[:, 1], data[:, 0], label='Data')
+    plt.plot(domain, domain*beta + b, label='LAD')
+
+    # Plot least-squares solution
+    slope, intercept = stats.linregress(data[:, 1], data[:, 0])[:2]
+    plt.plot(domain, domain*slope + intercept, label='Least-squares')
+
+    # Plot
+    plt.legend()
+    plt.show()
